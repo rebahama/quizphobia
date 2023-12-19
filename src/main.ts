6249eef59f66f53d1bb13edd2c3849ffb4c01fd9
CeciliaL1 <cecilia.lepik@hotmail.com>
2023-12-19 19:57:16 +0100

    feat(points): wrong answer

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ import './scss/style.scss'; // Importera huvud-SCSS-filen
 
 let score = 0;
 
+// let answerTime = ;     - Variable to use for the time it takes for user to answer question
+// let wrongAnswer = ;    - Boolean to use for wrong answer
+
 
 
 /******************************************************
@@ -20,24 +23,24 @@ let score = 0;
  *****************************************************/
 
 function getPointsForAnsweringQuestion(answerTime: number, wrongAnswer: boolean): void {
-  if (answerTime < 5) {
-    score = score + 150;
-   
-  } else if (answerTime >= 5 && answerTime < 10) {
-    score += 125;
-    
-  } else if (answerTime >= 10 && answerTime < 15) {
-    score += 100;
-    
-  } else {
-    score += 50;
-  }
   if (wrongAnswer) {
     score -= 30;
+    console.log(score);
+  } else if (answerTime < 5) {
+    score = score + 150;
+    console.log(score);
+  } else if (answerTime >= 5 && answerTime < 10) {
+    score += 125;
+    console.log(score);
+  } else if (answerTime >= 10 && answerTime < 15) {
+    score += 100;
+    console.log(score);
+  } else {
+    score += 50;
+    console.log(score);
   }
+
 }
-
-
 
 getPointsForAnsweringQuestion(answerTime, wrongAnswer); // passing the answerTime for each question as an argument 
 
