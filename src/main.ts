9c96f301c5679d876edb69643c59d50b34a0eb9e
CeciliaL1 <cecilia.lepik@hotmail.com>
2023-12-19 15:21:35 +0100

    feat(points)updated function

--- src/main.ts
+++ src/main.ts
@@ -19,21 +19,27 @@ let score = 0;
  * ************ Functions ****************************
  *****************************************************/
 
-function getPointsForAnsweringQuestion(answerTime: number): void {
-  if (answerTime > 5) {
+function getPointsForAnsweringQuestion(answerTime: number, wrongAnswer: boolean): void {
+  if (answerTime < 5) {
     score = score + 150;
-  } else if (answerTime < 5 && answerTime > 10) {
+   
+  } else if (answerTime >= 5 && answerTime < 10) {
     score += 125;
-  } else if (answerTime < 10 && answerTime > 15) {
+    
+  } else if (answerTime >= 10 && answerTime < 15) {
     score += 100;
+    
   } else {
     score += 50;
+  }
+  if (wrongAnswer) {
+    score -= 30;
   }
 }
 
 
 
-getPointsForAnsweringQuestion(answerTime); // passing the answerTime for each question as an argument 
+getPointsForAnsweringQuestion(answerTime, wrongAnswer); // passing the answerTime for each question as an argument 
 
 
 /******************************************************
