fd12420acf2dbfab69b16e1797f2b281c711aee5
RalfiSlask <nilssonnarki@gmail.com>
2023-12-22 12:08:58 +0100

    fix(eslint): fixed eslint multiline trailing commas to be more precise

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,37 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  "extends": [
-    "standard-with-typescript",
-    "prettier"
-  ],
-  "overrides": [
+  extends: ['standard-with-typescript', 'prettier'],
+  overrides: [
     {
-      "env": {
-        "node": true
+      env: {
+        node: true,
       },
-      "files": [
-        ".eslintrc.{js,cjs}"
-      ],
-      "parserOptions": {
-        "sourceType": "script"
-      }
-    }
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  "rules": {
+  rules: {
     '@typescript-eslint/comma-dangle': 'off',
     '@typescript-eslint/semi': 'off',
-    'arrow-spacing': ['error', { 'before': true, 'after': true }],
+    'arrow-spacing': ['error', { before: true, after: true }],
     'block-spacing': 'error',
-    'brace-style': ['error', '1tbs', { 'allowSingleLine': false}],
-    'comma-dangle': ['error', 'always-multiline'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+    'comma-dangle': ['error', 'only-multiline'],
     'curly': ['error', 'all'],
     'import/no-absolute-path': 'off',
     'indent': ['error', 2, { SwitchCase: 1 }],
-    'key-spacing': ['error', { 'afterColon': true, 'beforeColon': false }],
-    'keyword-spacing': ['error', { 'before': true }],
+    'key-spacing': ['error', { afterColon: true, beforeColon: false }],
+    'keyword-spacing': ['error', { before: true }],
     'max-len': ['error', { code: 120 }],
     'no-console': process.env.PROD ? 'warn' : 'off',
     'no-debugger': process.env.PROD ? 'warn' : 'off',
@@ -48,6 +43,6 @@ module.exports = {
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
     'space-before-blocks': ['error', 'always'],
-    'space-infix-ops': ['error', { 'int32Hint': false }]
-  }
-}
+    'space-infix-ops': ['error', { int32Hint: false }],
+  },
+};
